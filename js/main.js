df87abf66cfe7261bff9ad040928f50ada7658e6
yck-txt <yannickpuetz@protonmail.com>
2025-09-08 18:58:13 +0200

    keyboard info added; jquery slideshow

--- js/main.js
+++ js/main.js
@@ -15,3 +15,33 @@ document.addEventListener('DOMContentLoaded', function () {
 	});
 	observer.observe(nav);
 });
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let slides = $(".mySlides");
+  let dots = $(".dot");
+  console.log(slides.length);
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (let i = 0; i < slides.length; i++) {
+    console.log(slides[0])
+    $(slides[i]).css("display", "none");
+  }
+  for (let i = 0; i < dots.length; i++) {
+    $(dots[i]).className = $(dots[i]).removeClass("active");
+  }
+  $(slides[slideIndex-1]).css("display", "block");
+  $(dots[slideIndex-1]).addClass("active")
+} 
